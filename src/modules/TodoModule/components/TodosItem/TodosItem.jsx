1a6c7190caa76d7dc813ec37e81e7b1da6f29163
Albert <tonoyanalbert94@gmail.com>
2021-06-21 20:21:32 +0400

    some changes

--- src/modules/TodoModule/components/TodosItem/TodosItem.jsx
+++ src/modules/TodoModule/components/TodosItem/TodosItem.jsx
@@ -3,34 +3,65 @@ import './TodosItem.styles.scss';
 import Button from '@material-ui/core/Button';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
+import TextField from '@material-ui/core/TextField';
 
 const TodosItem = ({ remove, data, firebase }) => {
   const [currentName, setCurrentName] = useState(data.value.name);
   const [currentSurName, setCurrentSurName] = useState(data.value.surname);
+  const [nameErrorMessage, setNameErrorMessage] = useState(false);
+  const [surnameErrorMessage, setSurNameErrorMessage] = useState('');
 
   const [editMode, setEditMode] = useState(true);
 
   function handleEdit(id) {
+    if (!currentName) {
+      setNameErrorMessage(true);
+      if (!currentSurName) {
+        setSurNameErrorMessage(true);
+      }
+      return;
+    }
+    if (!currentSurName) {
+      setSurNameErrorMessage(true);
+      if (!currentName) {
+        setNameErrorMessage(true);
+      }
+      return;
+    }
     firebase.set(`todos/${id}`, { name: currentName, surname: currentSurName });
     setEditMode(!editMode);
   }
 
   return (
     <div className="tableItem">
-      <input
+      <TextField
         type="text"
         disabled={editMode}
         value={currentName}
+        error={nameErrorMessage}
         className={editMode && 'tableItemInput'}
-        onChange={({ target }) => setCurrentName(target.value)}
+        onChange={({ target }) => {
+          setNameErrorMessage(false);
+          setCurrentName(target.value);
+        }}
       />
-      <input
+      {nameErrorMessage && (
+        <span style={{ color: 'red' }}>Name is a required field*</span>
+      )}
+      <TextField
         type="text"
         disabled={editMode}
         value={currentSurName}
+        error={surnameErrorMessage}
         className={editMode && 'tableItemInput'}
-        onChange={({ target }) => setCurrentSurName(target.value)}
+        onChange={({ target }) => {
+          setSurNameErrorMessage(false);
+          setCurrentSurName(target.value);
+        }}
       />
+      {surnameErrorMessage && (
+        <span style={{ color: 'red' }}>Surname is a required field*</span>
+      )}
       {editMode ? (
         <Button
           onClick={() => setEditMode(false)}
